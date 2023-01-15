eda1a3b361fdf05987647ca29b742b828aec65d9
bielom43 <bielom43@gmail.com>
2023-01-15 11:27:55 -0300

    MediaQuery optmization

--- lib/main.dart
+++ lib/main.dart
@@ -101,7 +101,8 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    bool isLandscape = MediaQuery.of(context).orientation == Orientation.landscape;
+    final mediaQuery = MediaQuery.of(context);
+    bool isLandscape = mediaQuery.orientation == Orientation.landscape;
 
     final appBar = AppBar(
       title: const Text('Pessoal Expenses'),
@@ -124,8 +125,7 @@ class _MyHomePageState extends State<MyHomePage> {
       ],
     );
 
-    final availableHeight =
-        MediaQuery.of(context).size.height - appBar.preferredSize.height - MediaQuery.of(context).padding.top;
+    final availableHeight = mediaQuery.size.height - appBar.preferredSize.height - mediaQuery.padding.top;
 
     return Scaffold(
       appBar: appBar,
